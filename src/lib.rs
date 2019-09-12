ca398fb63b9b97f9e0fe420451b678149f879a9d
Matthew Nicholson <matt@matt-land.com>
2019-09-11 21:41:11 -0400

    add initial rust wasm skeleton

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+use wasm_bindgen::prelude::*;
+
+// Called when the wasm module is instantiated
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
