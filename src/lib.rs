86ddff60802e165dc0ceb1d0e35423d91de37ae0
Matthew Nicholson <matt@matt-land.com>
2019-09-11 22:25:16 -0400

    add initial wasm code

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,41 @@
 use wasm_bindgen::prelude::*;
+use cfg_if::cfg_if;
+use log::{debug,info};
 
-// Called when the wasm module is instantiated
+cfg_if! {
+    if #[cfg(feature = "console_error_panic_hook")] {
+        #[inline]
+        fn set_panic_hook() {
+            console_error_panic_hook::set_once();
+            debug!("panic hook set");
+        }
+    }
+    else {
+        fn set_panic_hook() {}
+    }
+}
+
+cfg_if! {
+    if #[cfg(feature = "console_log")] {
+        #[inline]
+        fn init_log() {
+            console_log::init_with_level(log::Level::Trace)
+                .expect("error initializing log");
+            debug!("log initialized");
+        }
+    }
+    else {
+        fn init_log() {}
+    }
+}
+
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
+    init_log();
+    set_panic_hook();
+
+    info!("Hello world!");
+    
     Ok(())
 }
 
